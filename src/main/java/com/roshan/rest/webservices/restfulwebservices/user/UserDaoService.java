e540fd840d32f4c8424558889a2c70454a148b7c
unknown <roushanraj395@gmail.com>
2023-07-03 16:07:57 +0530

    created users dao, service and methods to perform different actions on it

---
+++ src/main/java/com/roshan/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -0,0 +1,38 @@
+package com.roshan.rest.webservices.restfulwebservices.user;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDaoService {
+	
+	private static List<User> users = new ArrayList<>();
+	
+	private static Integer usersCount = 0;
+	
+	static {
+		users.add(new User(++usersCount, "roshan", LocalDate.now().minusYears(22)));
+		users.add(new User(++usersCount, "ronak", LocalDate.now().minusYears(30)));
+		users.add(new User(++usersCount, "roman", LocalDate.now().minusYears(20)));
+	}
+	
+	public List<User> findAll(){
+		return users;
+	}
+	
+	public User findOne(int id) {
+		Predicate<? super User> predicate = user -> user.getId().equals(id);
+		return users.stream().filter(predicate).findFirst().get();
+	}
+	
+	public User save(User user) {
+		user.setId(++usersCount);
+		users.add(user);
+		return user;
+	}
+
+}
