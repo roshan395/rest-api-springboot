174deb06b41755c738ac0738313921b25b055919
unknown <roushanraj395@gmail.com>
2023-07-05 10:59:05 +0530

    added validation

--- src/main/java/com/roshan/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/roshan/rest/webservices/restfulwebservices/user/User.java
@@ -2,10 +2,17 @@ package com.roshan.rest.webservices.restfulwebservices.user;
 
 import java.time.LocalDate;
 
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
+
 public class User {
 
 	private Integer id;
+	
+	@Size(min=2, message="name should have atleast 2 characters")
 	private String name;
+	
+	@Past(message="Birth date should be in past")
 	private LocalDate birthDate;
 	
 	public User(Integer id, String name, LocalDate birthDate) {
