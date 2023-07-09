d22ed102f871f52ef004cdf89b88d9aa861c0bdd
unknown <roushanraj395@gmail.com>
2023-07-09 13:01:44 +0530

    working satic data api

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into user_details(id, birth_date, name)
+values (1001, current_date(), 'ronak');
+
+insert into user_details(id, birth_date, name)
+values (1002, current_date(), 'rohan');
+
+insert into user_details(id, birth_date, name)
+values (1003, current_date(), 'roshan');
